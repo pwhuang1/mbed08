d7f6a80710692c9ddc7e3f7fafd962e50376e536
pwhuang1 <davidhpw568@gmail.com>
2021-04-21 01:48:32 -0700

    lab8

---
+++ src/BUILD/B_L4S5I_IOT01A/GCC_ARM/data_collect/BSP_B-L475E-IOT01/Drivers/BSP/Components/lps22hb/lps22hb.d
@@ -0,0 +1,6 @@
+BUILD/B_L4S5I_IOT01A/GCC_ARM/data_collect/BSP_B-L475E-IOT01/Drivers/BSP/Components/lps22hb/lps22hb.o: \
+ data_collect/BSP_B-L475E-IOT01/Drivers/BSP/Components/lps22hb/lps22hb.c \
+ BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed_config.h \
+ data_collect/BSP_B-L475E-IOT01/Drivers/BSP/Components/lps22hb/lps22hb.h \
+ data_collect/BSP_B-L475E-IOT01/Drivers/BSP/Components/lps22hb/../Common/psensor.h \
+ data_collect/BSP_B-L475E-IOT01/Drivers/BSP/Components/lps22hb/../Common/tsensor.h
